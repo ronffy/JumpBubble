b06742c5096d01c1d96021f6b1fcd4207b430744
wanghairong-i <3189578410@qq.com>
2017-08-02 14:55:02 +0800

    up note

--- src/js/JumpBubble.js
+++ src/js/JumpBubble.js
@@ -1,9 +1,143 @@
 /*
 ** JumpBubble.js 原生js实现气泡冒泡效果
-** 
-** from: https://github.com/wanghairong-i/
+** from: https://github.com/wanghairong-i/JumpBubble
 ** time:2016.12.12
 */
+
+/*
+** @param canvasNode   [必传] <DOM> canvan标签元素
+** @param config       [选传] <Obj> 可选配置项
+**    - effect      <Str> 气泡的y轴浮动效果,可选2个值：linear、ease，默认'ease'
+**    - speed       <Str> 气泡的Y轴移动速度，可选3个值 slow default fast, 默认'default'
+**    - isToAlapha  <Boo> 气泡是否逐渐增加透明度 默认true
+**    - alpha       <Num> 初始气泡的透明度， 默认0.9
+**    - max					<Num> //画布上最多同时存在多少气泡，默认30，请根据气泡出现频道调整此值，已达到最优的视觉效果
+**    - diffWidth   <Num> 初始冒泡时气泡宽度与指定宽度（width属性值）的差值，默认15，差值越大，冒泡时气泡从小变大的效果越明显
+**    - left        <Num> 冒泡位置，距离画布左侧像素数，默认在画布中间靠左15像素位置
+**    - top         <Num> 冒泡位置，距离画布顶部像素数，默认距离画布顶部为画布高度减30像素
+**    - width       <Num> 气泡的宽度， 默认30，为保证气泡不变形，高度随宽度改变
+** @param  callback    [选传] <Func> 态度气泡实例初始化后的回调
+*/
+export default class JumpBubble{
+	constructor(canvasNode, config = {}, callback ){
+		if(!canvasNode || !canvasNode.getContext){
+		  console.warn("jumpBuffle，启用失败，canvas传参错误 或 浏览器不支持canvas");
+		  this.error = true;
+		  return false;
+		}
+		const t = this,
+		    { width, height } = canvasNode;
+		const _config = { //配置气泡冒泡设置
+		  width: 30, //可自定义气泡宽度，高度随宽度变化
+		  left : width/2 - 15,  //距离左侧距离
+		  top : height - 30, //距离顶部距离
+		  alpha : 0.9,  // 透明度设置
+		  effect: 'ease',
+		  speed: 'default',
+		  max: 30, //画布上最多同时存在多少气泡， 默认30
+		  isToAlapha: true,
+		  diffWidth: 15, // 初始冒泡时气泡宽度与正常宽度的差值，默认15
+		  cavHeight: height,  // [非配置项] canvan标签的高度，设置气泡在不同高度有不同的浮动速度时会用到
+		  cavWidth: width,  	// [非配置项] canvan标签的高度，设置气泡在左右晃动，触壁反弹时会用到
+		};
+		Object.assign(t, {
+			canvasInfo: {
+				canvas : canvasNode,
+				width,
+				height
+			},
+			config: Object.assign(_config, config),
+			ctx: canvasNode.getContext("2d"),
+			bubbleArr: [],//用来存储所有的气泡
+			allImg: { //缓存创建的img标签
+				lists: [], //src的列表
+				doms: [], //缓存list对应的创建的dom
+				loadState: [], //是否img标签已经加载完毕，如果完毕再直接返回img标签的dom，如果没有加载完毕则放到img.load函数内返回imgdom
+			}
+		});
+		callback && callback(t);
+	}
+	/*
+	** 冒泡的生命周期
+	** before: 气泡开始冒泡前
+	** after: 单个气泡消失后
+	*/
+	create(imgsrc, before, after){
+		const t = this,
+					{ctx, error} = t;
+		if(!ctx || error){
+		  console.warn("jumpBuffle：create时，ctx错误");
+		  return false;
+		}
+		const { bubbleArr, canvasInfo } = t,
+		    	{ width: imgwidth, max } = t.config;
+		t.createImg(imgsrc).then(imgNode => {
+			const imgInfo = {
+			  el : imgNode,
+			  width : imgwidth || imgNode.width,
+			  height : imgwidth && imgNode.height*(imgwidth/imgNode.width) || imgNode.height
+			};
+			if(bubbleArr.length > max){
+			  return;
+			}
+			bubbleArr.push(new DrawImg(ctx, imgInfo, t.config));
+			//每添加一个气泡触发一次的回调函数, 生命周期为气泡开始冒泡前
+			before && before(canvasInfo.canvas, imgNode, bubbleArr);  
+			if(!t.setInter){
+			  t.setInterFn(after);
+			}
+		});
+		return this;
+	}
+	createImg(imgsrc){
+		return new Promise(res => {
+			const { lists, doms, loadState } = this.allImg;
+			const i = lists.indexOf(imgsrc);
+			if(i > -1 && loadState[i]){
+				res(doms[i]);
+				return ;
+			}
+			const img = document.createElement('img');
+			img.src = imgsrc;
+			img.setAttribute('style', 'display:none;');
+			document.body.appendChild(img);
+			lists.push(imgsrc);
+			doms.push(img);
+			img.onload = () => {
+				loadState.push(true);
+				res(img);
+			}
+		})
+	}
+	setInterFn(after){
+		const t = this, 
+					{ ctx, canvasInfo } = t,
+		    	{ width, height } = canvasInfo;
+		t.setInter = setInterval(function(){
+		  try{
+		  	ctx.clearRect(0, 0, width, height);
+		  	t.bubbleArr = t.bubbleArr.filter(function(val){
+		  	  val.addCtx();
+		  	  val.updateCtx();
+		  	  if(val.y < 10){
+		  	  	after && after();
+		  	    return false;
+		  	  }else{
+		  	    return true;
+		  	  }
+		  	});
+		  	if(t.bubbleArr.length === 0){
+		  	  clearInterval(t.setInter);
+		  	  t.setInter = null;
+		  	  ctx.clearRect(0, 0, width, height);
+		  	}
+		  }catch(e){
+		  	console.warn('创建态度气泡出错',e);
+		  	clearInterval(t.setInter);
+		  }
+		},25);
+	}
+}
 
 class DrawImg{
 	constructor(ctx, imgInfo, { left, top, alpha, speed, cavWidth, cavHeight, effect, isToAlapha, diffWidth }){
@@ -148,140 +282,3 @@ class DrawImg{
 		this.pullback();
 	}
 }
-
-export default class JumpBubble{
-	/*
-	** @param canvasNode   [必传] <DOM> canvan标签元素
-	** @param config       [选传] <Obj> 可选配置项
-	**    - effect      <Str> 气泡的y轴浮动效果,可选2个值：linear、ease，默认'ease'
-	**    - speed       <Str> 气泡的Y轴移动速度，可选3个值 slow default fast, 默认'default'
-	**    - isToAlapha  <Boo> 气泡是否逐渐增加透明度 默认true
-	**    - alpha       <Num> 初始气泡的透明度， 默认0.9
-	**    - max					<Num> //画布上最多同时存在多少气泡，默认30，请根据气泡出现频道调整此值，已达到最优的视觉效果
-	**    - diffWidth   <Num> 初始冒泡时气泡宽度与指定宽度（width属性值）的差值，默认15，差值越大，冒泡时气泡从小变大的效果越明显
-	**    - left        <Num> 冒泡位置，距离画布左侧像素数，默认在画布中间靠左15像素位置
-	**    - top         <Num> 冒泡位置，距离画布顶部像素数，默认距离画布顶部为画布高度减30像素
-	**    - width       <Num> 气泡的宽度， 默认30，为保证气泡不变形，高度随宽度改变
-	** @param  callback    [选传] <Func> 态度气泡实例初始化后的回调
-	*/
-	constructor(canvasNode, config = {}, callback ){
-		if(!canvasNode || !canvasNode.getContext){
-		  console.warn("jumpBuffle，启用失败，canvas传参错误 或 浏览器不支持canvas");
-		  this.error = true;
-		  return false;
-		}
-		const t = this,
-		    { width, height } = canvasNode;
-		const _config = { //配置气泡冒泡设置
-		  width: 30, //可自定义气泡宽度，高度随宽度变化
-		  left : width/2 - 15,  //距离左侧距离
-		  top : height - 30, //距离顶部距离
-		  alpha : 0.9,  // 透明度设置
-		  effect: 'ease',
-		  speed: 'default',
-		  max: 30, //画布上最多同时存在多少气泡， 默认30
-		  isToAlapha: true,
-		  diffWidth: 15, // 初始冒泡时气泡宽度与正常宽度的差值，默认15
-		  cavHeight: height,  // [非配置项] canvan标签的高度，设置气泡在不同高度有不同的浮动速度时会用到
-		  cavWidth: width,  	// [非配置项] canvan标签的高度，设置气泡在左右晃动，触壁反弹时会用到
-		};
-		Object.assign(t, {
-			canvasInfo: {
-				canvas : canvasNode,
-				width,
-				height
-			},
-			config: Object.assign(_config, config),
-			ctx: canvasNode.getContext("2d"),
-			bubbleArr: [],//用来存储所有的气泡
-			allImg: { //缓存创建的img标签
-				lists: [], //src的列表
-				doms: [], //缓存list对应的创建的dom
-				loadState: [], //是否img标签已经加载完毕，如果完毕再直接返回img标签的dom，如果没有加载完毕则放到img.load函数内返回imgdom
-			}
-		});
-		callback && callback(t);
-	}
-	/*
-	** 冒泡的生命周期
-	** before: 气泡开始冒泡前
-	** after: 单个气泡消失后
-	*/
-	create(imgsrc, before, after){
-		const t = this,
-					{ctx, error} = t;
-		if(!ctx || error){
-		  console.warn("jumpBuffle：create时，ctx错误");
-		  return false;
-		}
-		const { bubbleArr, canvasInfo } = t,
-		    	{ width: imgwidth, max } = t.config;
-		t.createImg(imgsrc).then(imgNode => {
-			const imgInfo = {
-			  el : imgNode,
-			  width : imgwidth || imgNode.width,
-			  height : imgwidth && imgNode.height*(imgwidth/imgNode.width) || imgNode.height
-			};
-			if(bubbleArr.length > max){
-			  return;
-			}
-			bubbleArr.push(new DrawImg(ctx, imgInfo, t.config));
-			//每添加一个气泡触发一次的回调函数, 生命周期为气泡开始冒泡前
-			before && before(canvasInfo.canvas, imgNode, bubbleArr);  
-			if(!t.setInter){
-			  t.setInterFn(after);
-			}
-		});
-		return this;
-	}
-	createImg(imgsrc){
-		return new Promise(res => {
-			const { lists, doms, loadState } = this.allImg;
-			const i = lists.indexOf(imgsrc);
-			if(i > -1 && loadState[i]){
-				res(doms[i]);
-				return ;
-			}
-			const img = document.createElement('img');
-			img.src = imgsrc;
-			img.setAttribute('style', 'display:none;');
-			document.body.appendChild(img);
-			lists.push(imgsrc);
-			doms.push(img);
-			img.onload = () => {
-				loadState.push(true);
-				res(img);
-			}
-		})
-	}
-	setInterFn(after){
-		const t = this, 
-					{ ctx, canvasInfo } = t,
-		    	{ width, height } = canvasInfo;
-		t.setInter = setInterval(function(){
-		  try{
-		  	ctx.clearRect(0, 0, width, height);
-		  	t.bubbleArr = t.bubbleArr.filter(function(val){
-		  	  val.addCtx();
-		  	  val.updateCtx();
-		  	  if(val.y < 10){
-		  	  	after && after();
-		  	    return false;
-		  	  }else{
-		  	    return true;
-		  	  }
-		  	});
-		  	if(t.bubbleArr.length === 0){
-		  	  clearInterval(t.setInter);
-		  	  t.setInter = null;
-		  	  ctx.clearRect(0, 0, width, height);
-		  	}
-		  }catch(e){
-		  	console.warn('创建态度气泡出错',e);
-		  	clearInterval(t.setInter);
-		  }
-		},25);
-	}
-}
-
-
