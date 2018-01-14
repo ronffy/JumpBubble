1f3f28faee6f54e6cb0bbdb2393602169355dba1
wanghairong-i <3189578410@qq.com>
2018-01-15 01:21:59 +0800

    up bubble to random show

--- src/index.js
+++ src/index.js
@@ -1,49 +1,21 @@
 import JumpBubble from './js/JumpBubble'
 import styles from './index.less'
 
-console.log(styles)
-
 let list1 = [
 	'http://p4.cdn.btime.com/t01e430315c854b44d2.png',
 	'http://p5.qhimg.com/t017f9904d4be818a87.png',
 	'http://p5.qhimg.com/t015ec16e404a442dd4.png',
 	'/img/fish.png', //注：路径是相对html的路径，因为该路径最终会放到img标签的src上
 ];
-let list2 = [
-	'http://p8.qhimg.com/t01053ab4d4d6510abd.png',
-	'http://p6.qhimg.com/t017895dcd6312beacb.png',
-	'http://p2.qhimg.com/t01f70bccf10e16addd.png',
-];
 
-const effects = ['ease', 'linear'];
-for(let [i, v] of effects.entries()){
-	const bubble = new JumpBubble(document.getElementById(`cavs${i}`),{
-		effect: v,
-		speed: i === 1 ? 'fast' : 'slow',
-		width: i === 0 ? 25 : 35,
-		isToAlapha: i === 0 ? false : true, //演示，第二个设置不进行透明度变化
-	});
-	let s = 0;
-	setInterval(() => {
-		if(s > list1.length - 1){
-			s = 0;
-		}
-		bubble.create(list1[s]);
-		s++
-	},250);
-}
+const bubble = new JumpBubble(document.getElementById(`cavs1`));
+console.log(bubble)
+let s = 0;
+setInterval(() => {
+	if (s > list1.length - 1) {
+		s = 0;
+	}
+	bubble.create(list1[s]);
+	s++
+}, 800);
 
-
-document.getElementById('btn-add').onclick = () => {
-	move(list1, list2);
-	return false;
-}
-document.getElementById('btn-del').onclick = () => {
-	move(list2, list1);
-	return false;
-}
-
-function move(target, source){
-	if(!(source.length - 1))return;
-	target.push(source.shift());
-}
--- src/js/JumpBubble.js
+++ src/js/JumpBubble.js
@@ -10,7 +10,6 @@ import styles from './jumpBubble.less'
 /*
 ** @param canvasNode   [必传] <DOM> canvan标签元素
 ** @param config       [选传] <Obj> 可选配置项
-**    - effect      <Str> 气泡的y轴浮动效果,可选2个值：linear、ease，默认'ease'
 **    - speed       <Str> 气泡的Y轴移动速度，可选3个值 slow default fast, 默认'default'
 **    - isToAlapha  <Boo> 气泡是否逐渐增加透明度 默认true
 **    - alpha       <Num> 初始气泡的透明度， 默认0.9
@@ -32,9 +31,7 @@ export default class JumpBubble{
 		    { width, height } = canvasNode;
 		const _config = { //配置气泡冒泡设置
 		  width: 30, //可自定义气泡宽度，高度随宽度变化
-		  left : width/2 - 15,  //距离左侧距离
-		  top : height - 30, //距离顶部距离
-		  alpha : 0.9,  // 透明度设置
+		  alpha : 1,  // 透明度设置
 		  effect: 'ease',
 		  speed: 'default',
 		  max: 30, //画布上最多同时存在多少气泡， 默认30
@@ -83,7 +80,11 @@ export default class JumpBubble{
 			if(bubbleArr.length > max){
 			  return;
 			}
-			bubbleArr.push(new DrawImg(ctx, imgInfo, t.config));
+			Object.assign(t.config, {
+				left: t.config.cavWidth - Math.max(Math.random() * t.config.cavWidth * 0.9, 30),
+				top: t.config.cavHeight - Math.max(Math.random() * t.config.cavHeight * 0.9, 30),
+			});
+			bubbleArr.push(new DrawImg(ctx, imgInfo, t.config)),
 			//每添加一个气泡触发一次的回调函数, 生命周期为气泡开始冒泡前
 			before && before(canvasInfo.canvas, imgNode, bubbleArr);  
 			if(!t.setInter){
@@ -122,7 +123,7 @@ export default class JumpBubble{
 		  	t.bubbleArr = t.bubbleArr.filter(function(val){
 		  	  val.addCtx();
 		  	  val.updateCtx();
-		  	  if(val.y < 10){
+					if (val.alpha < 0.01){
 		  	  	after && after();
 		  	    return false;
 		  	  }else{
@@ -138,7 +139,7 @@ export default class JumpBubble{
 		  	console.warn('创建态度气泡出错',e);
 		  	clearInterval(t.setInter);
 		  }
-		},25);
+		}, 400);
 	}
 }
 
@@ -149,8 +150,8 @@ class DrawImg{
 			ctx,
 			originWidth: imgInfo.width,
 			img: imgInfo.el,
-			imgWidth: imgInfo.width - diffWidth, //气泡初始大小与指定气泡大小的差值
-			imgHeight: imgInfo.height - diffWidth,
+			imgWidth: imgInfo.width, //气泡初始大小与指定气泡大小的差值
+			imgHeight: imgInfo.height,
 			x: left, //气泡在x轴的位置 相对左侧
 			y: top, //气泡在y轴位置 相对顶部
 			alpha,
@@ -166,17 +167,6 @@ class DrawImg{
 			diffAlapa: null, //缓存气泡在变为透明时每次增加的透明度
 		});
 		this.updateCtx = this.updateCtx.bind(this);
-		this.effectCommon = this.effectCommon.bind(this);
-	}
-	getSpeed(type = 'default'){
-		switch(type){
-			case 'slow':
-				return 0.6;
-			case 'fast':
-				return 1.4;
-			default:
-				return 1;
-		}
 	}
 	addCtx(){
 		const p = this, 
@@ -186,102 +176,11 @@ class DrawImg{
 		ctx.drawImage(p.img, p.x, p.y, p.imgWidth, p.imgHeight);
 		ctx.restore();
 	}
-	setImgWidth(){
-		const { originWidth, imgWidth } = this;
-		if(imgWidth < originWidth){//差值diffWidth的初始小气泡，逐渐变大为指定气泡的大小
-		  this.imgWidth += 1;
-		  this.imgHeight += 1;
-		}
-	}
-	setAlapa(){
-		const p = this,
-					{ y, isToAlapha, whichUnit } = p;
-		if(!isToAlapha)return false; //可自行配置，是否逐渐增加透明度
-		let diffAlapa = p.diffAlapa;
-		if(whichUnit === 2){ //气泡在画布中上部分时
-			if(!diffAlapa){
-				p.diffAlapa = diffAlapa = p.countDiffAlapa();
-			}
-		  if(p.alpha <= diffAlapa){
-		    p.alpha = 0;
-		  }else{
-		    p.alpha -= diffAlapa;
-		  }
-		}
-	}
-	countDiffAlapa(){
-		const { alpha, oneUnit, yPx } = this;
-		return (alpha + 0.1)/(oneUnit/yPx);
-	}
-	setYpx(){
-		const p = this,
-					{ y, oneUnit } = p;
-		let i;
-		// 根据态度气泡在容器内到达高度不同，设置不同的速度
-		switch(true){ 
-			case y < oneUnit: //气泡在画布中上部分时
-				i = 2;
-				break;
-			case y > oneUnit && y < oneUnit*2: //气泡在画布的中部分时
-				i = 1;
-				break;
-			default: //气泡在画布的中下部分
-				i = 0;
-		}
-		if(p.whichUnit !== i){
-			p.yPx = p.yPxArr[i];
-			p.whichUnit = i;
-		}
-		p.y -= p.yPxArr[i];
-	}
-	setYpxArr(){
-		if(this.yPxArr)return false;
-		this.yPxArr = this.countYpxArr(this.effect, this.getSpeed(this.speed));
-	}
-	countYpxArr(effect, speedNum = 1){//根据effect不同，设置气泡在y轴上位移距离，数组包含3个值，分别表示在y轴的3个阶段的位移距离
-		const basePx = 2;
-		const easeArr = [basePx-0.5, basePx, basePx+0.5];
-		const linearArr = [basePx, basePx, basePx];
-		switch(effect){
-			case 'ease':
-				return mlt(easeArr);
-			case 'linear':
-				return mlt(linearArr);
-			default:
-				return mlt(easeArr);
-		}
-		function mlt(arr){
-			return arr.map(v => v*speedNum);
-		}
-	}
-	pullback(){//在x轴上，触壁反弹效果，晃动的上升，
-		const p = this,
-					{ y, cavWidth, xPx, originWidth } = p;
-		// 控制气泡左右晃动，触壁反弹效果
-		switch(true){
-			case (p.x + originWidth >= cavWidth): //气泡触右侧壁
-				p.toRight = false;
-				p.x -= xPx;
-				break;
-			case p.x <= 2: //气泡触左侧壁
-				p.toRight = true;
-				p.x += xPx;
-				break;
-			case p.toRight:
-				p.x += xPx;
-				break;
-			default:
-				p.x -= xPx;
-		}
-		this.effectCommon();
-	}
-	effectCommon(){
-		this.setYpxArr();
-		this.setYpx();
-		this.setAlapa();
-		this.setImgWidth();
-	}
 	updateCtx(){
-		this.pullback();
+		if(this.alpha > 0.9){
+			this.alpha -= 0.01;
+		}else{
+			this.alpha = 0;
+		}
 	}
 }
